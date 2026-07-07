c110ebec29468b7a3d83ee621aa1cf1fd78b61a2
404DaniNotFound <dxdz3@outlook.com>
2026-07-07 17:12:16 -0300

    Funciones de búsqueda y utilidad
    Se implementaron funciones para buscar estudiantes por su DNI y cursos por su ID dentro de sus respectivas listas. Además, se incorporó una función de utilidad para limpiar la consola, adaptándose al sistema operativo en el que se ejecuta el programa.

--- tp_inscripciones_cursos/modulos/utilidades.py
+++ tp_inscripciones_cursos/modulos/utilidades.py
@@ -1,1 +1,19 @@
-[Dani]
+import os
+
+def buscar_estudiante_por_dni(lista_estudiantes, dni):
+	for estudiante in lista_estudiantes:
+		if estudiante["dni"] == dni:
+			return estudiante
+	return None
+
+def buscar_curso_por_id(lista_cursos, id_curso):
+	for curso in lista_cursos:
+		if curso["id"] == id_curso:
+			return curso
+	return None
+
+def limpiar_pantalla():
+	if os.name == 'nt':
+		os.system('cls')
+	else:
+		os.system('clear')

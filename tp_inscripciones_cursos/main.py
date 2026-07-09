f670d5cfa64c2f89c1d040283442423a267e7442
cchokyyy <alansitothegoblin@gmail.com>
2026-07-09 13:25:00 -0300

    Update main.py

--- tp_inscripciones_cursos/main.py
+++ tp_inscripciones_cursos/main.py
@@ -13,22 +13,29 @@ def menu_principal():
         print("\n=== SISTEMA DE INSCRIPCIÓN A CURSOS ===")
         print("1. Registrar Estudiante")
         print("2. Inscribir Estudiante a un Curso")
+        print("3. Dar de baja Estudiante a un Curso")
         print("3. Mostrar Estadisticas del Sistema")
         print("4. Salir")
         
-        opcion = input("Seleccione una opción (1-4): ")
+      opcion = input("\nSeleccione una opción (1-5): ")
         
-        if opcion == "1":
-            registrar_estudiante(estudiantes)
-        elif opcion == "2":
-            inscribir_estudiante(estudiantes, cursos)
-        elif opcion == "3":
-            mostrar_estadisticas(estudiantes, cursos)
-        elif opcion == "4":
-            print("Saliendo del sistema. ¡Hasta luego!")
-            break
-        else:
-            print("Opción inválida. Intente de nuevo.")
+      if opcion == "1":
+        limpiar_pantalla()
+        registrar_estudiante(estudiantes)
+      elif opcion == "2":
+        limpiar_pantalla()
+        inscribir_estudiante(estudiantes, cursos)
+      elif opcion == "3":
+        limpiar_pantalla()
+        dar_de_baja_curso(estudiantes, cursos)
+      elif opcion == "4":
+        limpiar_pantalla()
+        mostrar_estadisticas(estudiantes, cursos)
+      elif opcion == "5":
+        print("Saliendo del sistema. ¡Hasta luego!")
+        break
+      else:
+        print("Opción inválida. Intente de nuevo.")
 
 if __name__ == "__main__":
     menu_principal()
